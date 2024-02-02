e00e2acf91b28a9db937c6e2cdf529586386d85c
Tristan Rechenberger <rechenberger@me.com>
2024-02-02 14:02:08 +0100

    🎉

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -6,6 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { cn } from '@/lib/utils'
 import { fetchGoodNews } from '@/server/fetchGoodNews'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -18,32 +19,43 @@ export default async function Page() {
   return (
     <>
       {/* <h1>News</h1> */}
-      <div className="grid grid-cols-3 gap-4">
-        {feed.map((item) => {
+      <div className="grid grid-cols-1 gap-4">
+        {feed.map((item, idx) => {
+          const isBig = idx === 0
           return (
             <Fragment key={item.guid}>
               <Link href={`/news/${item.guid}`}>
-                <Card className="overflow-hidden">
-                  <Image
-                    src={item.imgUrl}
-                    alt={item.title}
-                    unoptimized
-                    width={360}
-                    height={360}
-                    className="w-full "
-                  />
-                  <CardHeader>
-                    <CardTitle>{item.title}</CardTitle>
-                    <CardDescription>
-                      <LocalDateTime datetime={item.isoDate} />
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <p>
-                      <strong>{item.contentSnippet}</strong>
-                    </p>
-                    {/* <p className="mt-4 whitespace-pre-wrap">{item.content}</p> */}
-                  </CardContent>
+                <Card
+                  className={cn(
+                    'overflow-hidden',
+                    'flex flex-col',
+                    !isBig && 'md:flex-row',
+                  )}
+                >
+                  <div
+                    className={cn('aspect-video relative', !isBig && 'md:h-60')}
+                  >
+                    <Image
+                      src={item.imgUrl}
+                      alt={item.title}
+                      unoptimized
+                      fill
+                    />
+                  </div>
+                  <div>
+                    <CardHeader>
+                      <CardTitle>{item.title}</CardTitle>
+                      <CardDescription>
+                        <LocalDateTime datetime={item.isoDate} />
+                      </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                      <p>
+                        <strong>{item.contentSnippet}</strong>
+                      </p>
+                      {/* <p className="mt-4 whitespace-pre-wrap">{item.content}</p> */}
+                    </CardContent>
+                  </div>
                 </Card>
               </Link>
             </Fragment>
